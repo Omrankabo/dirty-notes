e6ba8151ed2787d99402d4e8594ef921a609782f
Omrankabo <omrankabo@gmail.com>
2023-12-09 18:35:47 +0200

    remove sections from home

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div className={`${theme? 'dark' : ''} font-primary text-text-base bg-background-base min-w-screen min-h-screen bggrid`}>
       <Nav/>
-      <div className='container md:mx-auto md:max-w-[684px] lg:max-w-[736px]  py-[85px] px-6 lg:px-2 min-h-screen'>
+      <div className='container md:mx-auto md:max-w-[684px] lg:max-w-[736px]  py-[85px] px-6 lg:px-2 min-h-fit'>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/notes' element={<Notes/>}/>
