f242aa37cf98741d58edb1db0ebe9cf454d62121
Omrankabo <omrankabo@gmail.com>
2023-12-10 13:39:47 +0200

    remove images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import pic from '../assets/imgs/pic3.jpg'
+import { IoMdArrowForward } from "react-icons/io";
+
 const Card = () => {
   return (
     // TODO: change into react-router link to this particular topic
       <div class=" border-2 border-secondary-100 border-opacity-60 rounded-lg overflow-hidden">
-        <img loading='lazy' class="object-center h-36 md:h-36 w-full object-cover " src={pic} alt="blog"/>
         <div class="p-6 bg-background-50">
           <h2 class="tracking-widest text-xs title-font font-medium text-accent mb-1">#Culculus</h2>
           <h1 class="title-font text-lg font-medium text-text-base mb-2">Horizontal asymptotes</h1>
           <p class="leading-relaxed mb-2 text-sm text-text-800">How to evaulate vertical and horizontal asymptotes, and how to show them in a graph.</p>
           <div class="flex items-center flex-wrap">
-              <a class="text-text-base inline-flex items-center md:mb-2 lg:mb-0" href='/'>Learn More
-              <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="M5 12h14"></path>
-                  <path d="M12 5l7 7-7 7"></path>
-              </svg>
+              <a class="text-text-base inline-flex items-center gap-2 md:mb-2 lg:mb-0" href='/'>Learn More
+              <IoMdArrowForward/>
               </a>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,10 @@ const Home = () => {
   return (
     <main>
       <div className="flex flex-col md:items-center justify-center gap-2 my-8 h-[75vh] md:max-w-[584px] mx-auto  ">
-        <h1 class="sm:text-6xl md:text-7xl text-5xl mb-2 font-medium md:text-center ">
-          Get some Magical
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-base to-secondary-base"> Notes</span> to study better!. 
+        <h1 class="sm:text-6xl md:text-7xl text-5xl mb-2 font-medium md:text-center uppercase ">
+          Get some Magical <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-base to-secondary-base">
+            Notes
+          </span> to study better!. 
           
         </h1>
         <p className="mb-4 text-sm md:text-center">
@@ -15,7 +16,6 @@ const Home = () => {
           <Link to='/notes' className="w-full px-2 py-2 text-center rounded-md bg-accent text-text-100" role="button">Begin your journey</Link>
           <Link to='/contact' className="w-full px-2 py-2 text-center rounded-md bg-secondary-base text-text-100" role="button">Give us some feedback</Link>
         </div>
-        {/* <div> Scroll dowm to see more</div> */}
       </div>
     </main>
   )
